8b040cccf784ec2fe9a506e2a448fd378928cc7d
Ilikegoogology <142138596+Ilikegoogology@users.noreply.github.com>
2025-06-07 13:21:24 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -4,7 +4,12 @@ const mod = {
     version: "1.0.0",
     engineVer: "1.0.1", //DO NOT MODIFY
     debugMode: false,
-    Infinities: [new Decimal(2).pow(1024), new Decimal("1.8e30008"), new Decimal("1.8e300000008"), new Decimal("ee38")],
+    Infinities: [
+        new Decimal(2).pow(1024), 
+        new Decimal("1.8e30008"), 
+        new Decimal("1.8e300000008"), 
+        new Decimal("ee38")
+    ],
     themes: [
         ["Dark", "css/themes/dark.css"],
         ["Dark Alt (by Jeehan2561)", "css/themes/darkalt.css"],
@@ -17,41 +22,57 @@ const mod = {
     ],
     layerNames: [
         ["russian",
-        [
-            "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
-            "ѣѳѵѡѫѭѣ́",
-            ["<span class='flipped-v'>Ꙉ</span>", "<span class='flipped-v'>Ω</span><sup>2</sup>","<span class='flipped-v'>Ω</span><sup>3</sup>","<span class='flipped-v'>Ω</span><sup>2<sup>2</sup></sup>"]
-        ]],vvv:l
+            [
+                "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
+                "ѣѳѵѡѫѭѣ́",
+                [
+                    "<span class='flipped-v'>Ꙉ</span>", 
+                    "<span class='flipped-v'>Ꙉ</span><sup>2</sup>",
+                    "<span class='flipped-v'>Ꙉ</span><sup>3</sup>",
+                    "<span class='flipped-v'>Ꙉ</span><sup>2<sup>2</sup></sup>"
+                ]
+            ]
+        ],
         ["Latin",
-        [
-            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-            "ðþȝƿəŋſÐÞȜǷƏŊ",
-            "æœĳǉ"
-        ]],
+            [
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                "ðþȝƿəŋſÐÞȜǷƏŊ",
+                "æœĳǉ"
+            ]
+        ],
         ["Alphabet",
-        [
-            "abcdefghijklmnopqrstuvwxyz",
-            "123456789",
-            "ABCD"
-        ]],
+            [
+                "abcdefghijklmnopqrstuvwxyz",
+                "123456789",
+                "ABCD"
+            ]
+        ],
         ["Symbols",
-        [
-            '!"£$%^&*;:@',
-            "<,[{}].>",
-            "+×÷^"
-        ]],
+            [
+                '!"£$%^&*;:@',
+                "<,[{}].>",
+                "+×÷^"
+            ]
+        ],
         ["Binary",
-        [
-            '01',
-            "01",
-            "2345"
-        ]],
+            [
+                '01',
+                "01",
+                "2345"
+            ]
+        ],
         ["Random",
-        [
-            Utils.createRandomWord(10, new Random(Date.now()).nextInt()),
-            Utils.createRandomWord(10, new Random(Math.floor(Date.now()/2)).nextInt()),
-            [Utils.createRandomWord(2, new Random(Math.floor(Date.now()/3)).nextInt()),Utils.createRandomWord(3, new Random(Math.floor(Date.now()/4)).nextInt()),Utils.createRandomWord(4, new Random(Math.floor(Date.now()/5)).nextInt()),Utils.createRandomWord(5, new Random(Math.floor(Date.now()/6)).nextInt())]
-        ]]
+            [
+                Utils.createRandomWord(10, new Random(Date.now()).nextInt()),
+                Utils.createRandomWord(10, new Random(Math.floor(Date.now()/2)).nextInt()),
+                [
+                    Utils.createRandomWord(2, new Random(Math.floor(Date.now()/3)).nextInt()),
+                    Utils.createRandomWord(3, new Random(Math.floor(Date.now()/4)).nextInt()),
+                    Utils.createRandomWord(4, new Random(Math.floor(Date.now()/5)).nextInt()),
+                    Utils.createRandomWord(5, new Random(Math.floor(Date.now()/6)).nextInt())
+                ]
+            ]
+        ]
     ],
     layerNamePlaceholder: "𐌒",
     fonts: [
@@ -62,19 +83,19 @@ const mod = {
         ["Roboto", "css/fonts/roboto.css"],
         ["Ubuntu", "css/fonts/ubuntu.css"],
         ["Comfortaa", "css/fonts/comfortaa.css"],
-        ["Minecraft", "css/fonts/minecraft.css"],
+        ["Minecraft", "css/fonts/minecraft.css"]
     ],
     saves: [
         ["Save 1", ""],
         ["Save 2", "2"],
         ["Save 3", "3"],
-        ["Save 4", "4"],
+        ["Save 4", "4"]
     ],
     debugClasses: []
 }
 
-//DO NOT MODIFY CODE PAST THIS POINT AS IT IS NEEDED (unless your a pro coder then do some experimenting)
+// DO NOT MODIFY CODE PAST THIS POINT AS IT IS NEEDED (unless you're a pro coder then do some experimenting)
 
-mod.layerNames.push(["Refresh Names", "refresh"])
+mod.layerNames.push(["Refresh Names", "refresh"]);
 
-document.getElementById("superImportantTitle").innerHTML = "<span class='omega'>"+mod.primaryName+"</span>"+mod.secondaryName
+document.getElementById("superImportantTitle").innerHTML = "<span class='omega'>" + mod.primaryName + "</span>" + mod.secondaryName;
